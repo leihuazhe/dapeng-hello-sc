3cb8e156bd3204db3de0acbdb5d75180bb9a27e7
StruggleYang <1724555319@qq.com>
2018-07-09 00:33:59 +0800

    client-test

--- hello-api/src/main/resources/thrifts/hello_service.thrift
+++ hello-api/src/main/resources/thrifts/hello_service.thrift
@@ -8,14 +8,56 @@ namespace java com.dapeng.example.hello.service
 service HelloService {
 
 /**
-# say hello
-
-**/
+# sayHello
+## 业务描述
+    sayHello
+## 接口依赖
+    无
+## 边界异常说明
+    无
+## 输入
+    hello
+## 前置检查
+    无
+## 权限检查
+    无
+## 逻辑处理
+    sayHello
+## 数据库变更
+    无
+## 事务处理
+    无
+## 输出
+    string
+## 事件
+    无
+*/
     string sayHello(1:string name),
 /**
-# say hello2
-
-**/
+# sayHello
+## 业务描述
+    sayHello
+## 接口依赖
+    无
+## 边界异常说明
+    无
+## 输入
+    hello
+## 前置检查
+    无
+## 权限检查
+    无
+## 逻辑处理
+    sayHello
+## 数据库变更
+    无
+## 事务处理
+    无
+## 输出
+    string
+## 事件
+    无
+*/
     string sayHello2(1:hello_domain.Hello hello)
 
 }(group="hello")
